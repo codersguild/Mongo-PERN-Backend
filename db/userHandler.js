2962a24fbf2ad678d2ba89f077cf669d3be752cb
Sumit Lahiri <sumitl@cse.iitk.ac.in>
2020-05-24 01:12:47 +0530

    Update userHandler.js

--- db/userHandler.js
+++ db/userHandler.js
@@ -2,7 +2,7 @@
 import QUERIES from "./query";
 import db from "./queryHandler";
 
-export const allUsers = async () => {
-    let data = await db.query(QUERIES.FETCH_ALL_USERS)
-    return data
+export const allData = async () => {
+    let data = await db.query(QUERIES.FETCH_ALL_DATA)
+    return data.rows
 }
