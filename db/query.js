1b58228e1dca7b042504b50b6850bab8c6db02f9
Sumit Lahiri <sumitl@cse.iitk.ac.in>
2020-05-23 19:19:29 +0530

    Update query.js

--- db/query.js
+++ db/query.js
@@ -1,6 +1,4 @@
 export default ({
-    FETCH_ALL_USERS: "SELECT * FROM users"
-    // Add more Queries here. 
-});
-  
-
+    FETCH_ALL_DATA: "SELECT * FROM sampletable"
+    // Add more queries here. 
+  });
